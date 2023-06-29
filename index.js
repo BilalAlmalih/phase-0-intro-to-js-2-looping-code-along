d957b374b095e38ae6b7ed023a911af1b4be27ae
BilalAlmalih <belalmaleh14@gmail.com>
2023-06-29 17:43:02 +0200

    done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number);
+    number--;
+  }
+}
+function writeCards(names) {
+    const thankYouMessages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      const message = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+      thankYouMessages.push(message);
+    }
+  
+    return thankYouMessages;
+  }
